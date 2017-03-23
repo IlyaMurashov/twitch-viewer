7ce71d0d6633e1321ef60c308e9c0637c1fda9bb
IlyaMurashov <murashov.ilya@gmail.com>
2017-03-23 17:48:46 +0100

    Before refactoring API

--- src/api/twitchApi.js
+++ src/api/twitchApi.js
@@ -11,3 +11,23 @@ export function queryUser(user) {
 
   return jsonpRequest.promise;
 }
+
+export function queryAll(user) {
+  const userJsonp = jsonp(`https://wind-bow.glitch.me/twitch-api/users/${user}`);
+  const channelJsonp = jsonp(`https://wind-bow.glitch.me/twitch-api/channels/${user}`);
+  const streamJsonp = jsonp(`https://wind-bow.glitch.me/twitch-api/streams/${user}`);
+
+  return Promise.all(
+    [userJsonp.promise, channelJsonp.promise, streamJsonp.promise]
+      .map(p => p.catch(() => ({error: "Error fetching data"})))
+  )
+    .then(results => {
+      let response = {};
+
+      response.user = results[0];
+      response.channel = results[1];
+      response.stream = results[2];
+
+      return response;
+    });
+}
--- src/containers/ChannelsContainer.js
+++ src/containers/ChannelsContainer.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import {queryStream, queryUser} from '../api/twitchApi';
+import {queryAll, queryStream, queryUser} from '../api/twitchApi';
+import {NotFoundChannelCard} from '../components/NotFoundChannelCard';
+import {IdleChannelCard} from '../components/IdleChannelCard';
+import {StreamingChannelCard} from '../components/StreamingChannelCard';
 
 export default class ChannelsContainer extends React.Component {
   constructor(props, context) {
     super(props, context);
+
+    queryAll('brunofin').then((data) => {console.log(data);});
 
     this.state = {
       trackedChannels: ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin"],
@@ -13,12 +18,44 @@ export default class ChannelsContainer extends React.Component {
     };
   }
 
+  addNotFoundUser(user) {
+    const newCard = <NotFoundChannelCard name={user} key={user}/>;
+
+    this.setState(prevState => ({
+      notFoundChannels: [...prevState.notFoundChannels, newCard]
+    }));
+  }
+
+  addIdleStream(stream) {
+    const newCard = <IdleChannelCard
+      name={stream['stream']['channel']['display_name']}
+      link=""
+      key={stream['stream']['channel']['display_name']}
+    />;
+
+    this.setState(prevState => ({
+      idleChannels: [...prevState.idleChannels, newCard]
+    }));
+  }
+
+  addActiveStream(stream) {
+    const newCard = <StreamingChannelCard
+      name={stream['stream']['channel']['display_name']}
+      link=""
+      key={stream['stream']['channel']['display_name']}
+    />;
+
+    this.setState(prevState => ({
+      streamingChannels: [...prevState.streamingChannels, newCard]
+    }));
+  }
+
   // TODO: move it to constructor()
   componentWillMount() {
     const userMap = {};
-    const notFoundChannels = [];
-    const idleChannels = [];
-    const streamingChannels = [];
+    const notFoundUsers = [];
+    const idleStreams = [];
+    const activeStreams = [];
     this.state.trackedChannels.forEach((ch) => {
       userMap[ch] = [queryUser(ch), queryStream(ch)];
     });
@@ -27,7 +64,8 @@ export default class ChannelsContainer extends React.Component {
       userMap[user][0]
         .then((userData) => {
           if (userData.error) {
-            notFoundChannels.push(user);
+            notFoundUsers.push(user);
+            this.addNotFoundUser(user);
             console.log("Not found: " + user);
             return Promise.reject();
           }
@@ -38,11 +76,13 @@ export default class ChannelsContainer extends React.Component {
         .then(
           (streamData) => {
             if (streamData.stream === null) {
-              idleChannels.push(user);
+              idleStreams.push(streamData);
+              this.addIdleStream(streamData);
               console.log("Idle: " + user);
             }
             else {
-              streamingChannels.push(user);
+              activeStreams.push(streamData);
+              this.addActiveStream(streamData);
               console.log("Streaming: " + user);
             }
           },
@@ -51,17 +91,48 @@ export default class ChannelsContainer extends React.Component {
           });
     }
 
+    notFoundUsers.sort((u1, u2) => u1.toLowerCase().localeCompare(u2.toLowerCase()));
+    idleStreams.sort((s1, s2) =>
+      s1['channel']['display_name'].toLowerCase().localeCompare(s2['channel']['display_name']));
+    activeStreams.sort((s1, s2) =>
+      s1['channel']['display_name'].toLowerCase().localeCompare(s2['channel']['display_name']));
+
+    const notFoundUserCards = notFoundUsers.map(u => (
+      <NotFoundChannelCard
+        name={u}
+        key={u}
+      />
+    ));
+
+    const idleCards = idleStreams.map(s => (
+      <IdleChannelCard
+        name={s['channel']['display_name']}
+        link=""
+        key={s['channel']['display_name']}
+      />
+    ));
+
+    const streamingCards = activeStreams.map(s => (
+      <StreamingChannelCard
+        name={s['channel']['display_name']}
+        link=""
+        key={s['channel']['display_name']}
+      />
+    ));
+
     this.setState({
-      notFoundChannels: notFoundChannels,
-      idleChannels: idleChannels,
-      streamingChannels: streamingChannels
+      notFoundChannels: notFoundUserCards,
+      idleChannels: idleCards,
+      streamingChannels: streamingCards
     });
   }
 
   render() {
     return (
       <div className="channels-container">
-
+        {this.state.streamingChannels}
+        {this.state.idleChannels}
+        {this.state.notFoundChannels}
       </div>
     );
   }
